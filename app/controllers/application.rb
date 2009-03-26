fbb9121999f815b5909f7ceadaae76c3b10d9bad
Sven Suska <sound@minthink.(none)>
2009-03-26 04:22:52 +0100

    Kompatibilität zu Rails 2.3 hergestellt (application.rb --> application_controller.rb)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,15 +1,1 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
-class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :only => [:update, :destroy] # :secret => '4144dba2356ef11b1771b83d248a0180'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
-end
+require "application_controller"
