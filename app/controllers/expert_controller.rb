80afb8165bafece727a593c66ce6078c9292a506
Sven Suska <sven828gh@suska.org>
2009-04-21 22:44:36 +0200

    Expert-Modus jetzt prinzipiell über before_filter. Ajax-Variante auch weiter gepflegt. Nur Variante mit Reload vom Controller aus ist noch nicht fertig.

--- app/controllers/expert_controller.rb
+++ app/controllers/expert_controller.rb
@@ -1,8 +1,15 @@
 class ExpertController < ApplicationController
   def schalten
     gefunden = (params[:zustand] =~ /ein/)
+    p "EXPERTMODUS #{gefunden}"
     session[:expert_modus] = gefunden ? true : false
+    warn "EXPERTMODUS #{session[:expert_modus]}"
     #render :text => "Expertmodus #{session[:expert_modus]}"
-    render :action => "expert_bereich", :layout => false
+    if request.xhr? then
+      render :partial => "expert_bereich", :layout => false
+    else
+      redirect_to request.request_uri
+    end
   end
+
 end
