a4d663b63ff6dfcbb1e33e6d0227127c38457aaa
Sven Suska <sven828gh@suska.org>
2009-09-16 08:33:21 +0200

    navi_dauern in diagramme_helper verlagert

--- app/helpers/diagramme_helper.rb
+++ app/helpers/diagramme_helper.rb
@@ -1,2 +1,9 @@
 module DiagrammeHelper
+  def navi_dauern(zeit)
+   dauern = [zeit.laenger, zeit, zeit.kuerzer].map(&:dauer)
+   dauern += [1.weeks, 12.hours]
+   dauern = dauern.uniq.sort.reverse
+   dauern += [nil] if dauern.size < 5
+   dauern
+  end
 end
