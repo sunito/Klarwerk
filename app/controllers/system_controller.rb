1563946932ba6053eb72e26c574e5607ee564359
Marvin Benzmann <marvin.krueger@yahoo.de>
2016-03-02 12:48:53 +0100

    Systemverwaltung ausgebaut. Abfrage der Stati von gods Diensten und Neustart-, sowie Start- und Stop-Funktion via Ajax realisiert. Bug: Aktualisierung der Systemverwaltung via jQuery beim Aufruf der  Neustart-Funktion funktioniert nicht.

--- app/controllers/system_controller.rb
+++ app/controllers/system_controller.rb
@@ -12,8 +12,50 @@ class SystemController < ApplicationController
   def index
   	@dienste = DIENSTE.map do |dienst_name|
   	  status_abfrage_erg = "on"
-  	  dienst_status = (status_abfrage_erg == "on") 
+      god_status = (`god status #{dienst_name}`)
+      god_status = god_status.split(':')[1]
+      p god_status
+      if god_status == nil
+        dienst_status = false
+      elsif god_status.include? "up"
+        dienst_status = true
+      elsif god_status.include? "unmonitored"
+        dienst_status = false
+        
+      end
   	  Dienst.new(dienst_name, dienst_status)
   	end
+  end
+
+  def ajax_update
+    p params[:action]
+    if(params[:action] == "restart")
+      `god restart #{params[:dienst]}`
+    end
+    p params[:dienst]
+    god_dienste = `god status`.chomp("\n") 
+    p god_dienste
+    render :action => "datenverarbeiter.js.erb",  :layout => false, :locals => {:god_status => god_dienste}
+  end
+  def ajax_dienst_neustarten
+    god_status = `god stop #{params[:dienst]}`
+    render :action => "datenverarbeiter.js.erb",  :layout => false, :locals => {:god_status => god_status}
+  end
+  def get_status
+    god_status = `god status #{params[:dienst]}`
+  end
+  def ajax_dienst_start_stop
+    if `god status #{params[:dienst]}`.include? "up"
+      p '#{params[:dienst]} wird gestoppt'
+      `god stop #{params[:dienst]}`
+    else
+      p '#{params[:dienst]} wird gestartet'
+      `god start #{params[:dienst]}`
+    end
+    render :action => "datenverarbeiter.js.erb",  :layout => false
+  end
+  def ajax_dienst_neustart
+    `god restart #{params[:dienst]}`
+    render :action => "datenverarbeiter.js.erb",  :layout => false
   end
 end
